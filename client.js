e952f45545337420847d3e71967035d58eb70022
Amie Thao <amie.c.thao@gmail.com>
2018-11-18 14:47:04 -0600

    add changes to swap and delete buttons

--- client.js
+++ client.js
@@ -7,28 +7,39 @@ function nowReady(){
 
   // append a <div> element when you click the button
   $('#generateBtn').on('click', clickBtn);
-  $('#box').on('click', '#btnOne', swap);
 }
 
-let count = 0;
+let count = 1;
 
 // function adds box and display whats in the div box
 function clickBtn() {
   console.log('btn is clicked');
-
-  let divBox = '<div id="childBox"><p></p><button id="btnOne">Swap</button> <button class="btnTwo">Delete</button></div>';
+  // creating mutliple child box so it must be class 
+  let divBox = `<div class="childBox"><p>${count}</p>
+                <button class="btn">Swap</button> 
+                <button class="btnDelete">Delete</button>
+                </div>`;
   
   // append to ID box
   $('#box').append(divBox);
   count ++;
-  
   console.log(count);
-  $('p').append(count);
+
+  // click available only the childbox is generated in DOM.\
+  $('#box').on('click', '.btn', swap);
+  $('#box').on('click', '.btnDelete', deleteBtn);
+ 
 }
 
 // when swap is clicked the background-color turns yellow
 function swap() {
   console.log('swap has been clicked');
+  $(this).parent().toggleClass('yellow');
 
-  $('#box').css('background-color', 'yellow');
+}
+
+// delete the child box
+function deleteBtn(){
+  console.log('delete has been clicked.');
+  $(this).parent().empty();
 }
